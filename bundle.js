cd1c7ded218e1d3780d03347ebeb5be1fda18f24
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-02 15:53:14 +0000

    hook up button to console log something

--- bundle.js
+++ bundle.js
@@ -1,2 +1,25 @@
 (() => {
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // scripts/postnotedata.js
+  var require_postnotedata = __commonJS({
+    "scripts/postnotedata.js"(exports, module) {
+      function postNoteData2() {
+        console.log("heelo");
+      }
+      module.exports = postNoteData2;
+    }
+  });
+
+  // index.js
+  var postNoteData = require_postnotedata();
+  var noteTitle = document.querySelector("#title");
+  var noteContent = document.querySelector("#note-content");
+  var btn = document.querySelector("#btn");
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    postNoteData();
+  });
 })();
---
+++ scripts/postnotedata.js
@@ -0,0 +1,5 @@
+function postNoteData() {
+  console.log("heelo");
+}
+
+module.exports = postNoteData;
